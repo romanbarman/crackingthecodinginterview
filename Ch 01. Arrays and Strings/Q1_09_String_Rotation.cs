87de522dbf929ac3e4de6f647e9ca9e307014ca7
Raman Zanko <r.zanko@godeltech.com>
2022-06-08 17:20:39 +0300

    Updated description for question 9 from chapter 1

--- Ch 01. Arrays and Strings/Q1_09_String_Rotation.cs
+++ Ch 01. Arrays and Strings/Q1_09_String_Rotation.cs
@@ -7,7 +7,8 @@ namespace Chapter01
     {
         public string GetDescription()
         {
-            return "Test";
+            return "Suppose there is an isSubstring method that checks if one word is a substring of another. "
+                + "Given two strings s1 and s2, write code that checks whether string s2 was obtained by cyclic shifting s1 using only one call to the isSubstring method.";
         }
 
         public void Run()
