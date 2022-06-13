5640b592ece7e6b8e6d13ce3058b3a85010c13c5
Raman Zanko <r.zanko@godeltech.com>
2022-06-13 09:07:27 +0300

    Added queue

---
+++ Structures/Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Structures
+{
+    public class Queue<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; }
+            public Node Next { get; set; }
+        }
+
+        private Node first;
+        private Node last;
+
+        public void Add(T value)
+        {
+            var node = new Node(value);
+
+            if (last != null)
+            {
+                last.Next = node;
+            }
+
+            last = node;
+
+            if (first == null)
+            {
+                first = last;
+            }
+        }
+
+        public T Remove()
+        {
+            if (first == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var result = first.Value;
+            first = first.Next;
+            if (first == null)
+            {
+                last = null;
+            }
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (first == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return first.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+    }
+}
