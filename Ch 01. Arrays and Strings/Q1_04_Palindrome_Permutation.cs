8462aa92fae65ac07d02df2e48c8129cd15f7a7a
Raman Zanko <r.zanko@godeltech.com>
2022-06-20 09:07:26 +0300

    Updated architecture for question 4 from chapter 1

--- Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation.cs
+++ Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation.cs
@@ -1,77 +1,31 @@
 ﻿using Contracts;
-using System;
 using System.Collections.Generic;
 
 namespace Chapter01
 {
-    public class Q1_04_Palindrome_Permutation : IQuestion
+    public class Q1_04_Palindrome_Permutation : BaseQuestion
     {
-        public string GetDescription()
+        private static ISolution[] solutions = new ISolution[]
         {
-            return "Write a function that checks if a given string is a permutation of a palindrome. "
-                + "A palindrome is not limited to dictionary words.";
-        }
-
-
-        public void Run()
-        {
-            var lines = new []
+            new PalindromePermutation.Solution(new []
             {
                 "Rats live on no evil star",
                 "A man, a plan, a canal, panama",
                 "Lleve",
                 "Tacotac",
                 "asda"
-            };
+            })
+        };
 
-            foreach (var line in lines)
-            {
-                Console.WriteLine($"{line}: IsPalindromePermutation -> {IsPalindromePermutation(line)}");
-            }
+        public override string GetDescription()
+        {
+            return "Write a function that checks if a given string is a permutation of a palindrome. "
+                + "A palindrome is not limited to dictionary words.";
         }
 
-        private bool IsPalindromePermutation(string line)
+        protected override IList<ISolution> GetSolutions()
         {
-            var countCharsResult = CountChars(line);
-
-            bool hasOneOddValue = false;
-
-            foreach (var keyValue in countCharsResult)
-            {
-                if (keyValue.Value % 2 != 0)
-                {
-                    if (hasOneOddValue)
-                    {
-                        return false;
-                    }
-
-                    hasOneOddValue = true;
-                }
-            }
-
-            return true;
-        }
-
-        private Dictionary<char, int> CountChars(string word)
-        {
-            var dictionary = new Dictionary<char, int>();
-
-            foreach (var ch in word.ToLower())
-            {
-                if (char.IsLetter(ch))
-                {
-                    if (dictionary.ContainsKey(ch))
-                    {
-                        dictionary[ch]++;
-                    }
-                    else
-                    {
-                        dictionary[ch] = 1;
-                    }
-                }
-            }
-
-            return dictionary;
+            return solutions;
         }
     }
 }
---
+++ Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/Solution.cs
@@ -0,0 +1,75 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter01.PalindromePermutation
+{
+    public class Solution : ISolution
+    {
+        private string[] lines;
+
+        public Solution(string[] lines)
+        {
+            this.lines = lines;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine($"'{line}': IsPalindromePermutation -> {IsPalindromePermutation(line)}");
+            }
+        }
+
+        private bool IsPalindromePermutation(string line)
+        {
+            var countCharsResult = CountChars(line);
+
+            bool hasOneOddValue = false;
+
+            foreach (var keyValue in countCharsResult)
+            {
+                if (keyValue.Value % 2 != 0)
+                {
+                    if (hasOneOddValue)
+                    {
+                        return false;
+                    }
+
+                    hasOneOddValue = true;
+                }
+            }
+
+            return true;
+        }
+
+        private Dictionary<char, int> CountChars(string word)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+            foreach (var ch in word.ToLower())
+            {
+                if (char.IsLetter(ch))
+                {
+                    if (dictionary.ContainsKey(ch))
+                    {
+                        dictionary[ch]++;
+                    }
+                    else
+                    {
+                        dictionary[ch] = 1;
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
