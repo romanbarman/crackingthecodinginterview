6b591d732df4a584efb8dd4fda4f60b632b8c79d
Raman Zanko <r.zanko@godeltech.com>
2022-06-21 08:18:26 +0300

    Updated architecture for question 6 from chapter 1

--- Ch 01. Arrays and Strings/Q1_06_String_Compression.cs
+++ Ch 01. Arrays and Strings/Q1_06_String_Compression.cs
@@ -1,51 +1,24 @@
 ﻿using Contracts;
-using System;
-using System.Text;
+using System.Collections.Generic;
 
 namespace Chapter01
 {
-    public class Q1_06_String_Compression : IQuestion
+    public class Q1_06_String_Compression : BaseQuestion
     {
-        public string GetDescription()
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new StringCompression.Solution(new[] { "aabcccccaaad", "abcd", "aabbc" })
+        };
+
+        public override string GetDescription()
         {
             return "Implement a method to perform basic string compression using a repeated character count. "
                 + "If the compressed string does not become shorter than the original string, then the method returns the original string.";
         }
 
-        public void Run()
+        protected override IList<ISolution> GetSolutions()
         {
-            var words = new[] { "aabcccccaaad", "abcd", "aabbc" };
-
-            foreach (var word in words)
-            {
-                Console.WriteLine($"{word}: Compression -> {Compression(word)}");
-            }
-        }
-
-        private string Compression(string original)
-        {
-            var sb = new StringBuilder();
-
-            int count = 1;
-            sb.Append(original[0]);
-
-            for (var i = 1; i < original.Length; i++)
-            {
-                if (original[i - 1] == original[i])
-                {
-                    count++;
-                }
-                else
-                {
-                    sb.Append(count.ToString());
-                    sb.Append(original[i]);
-                    count = 1;
-                }
-            }
-
-            sb.Append(count);
-
-            return sb.Length < original.Length ? sb.ToString() : original;
+            return solutions;
         }
     }
 }
---
+++ Ch 01. Arrays and Strings/Q1_06_String_Compression/Solution.cs
@@ -0,0 +1,57 @@
+﻿using Contracts;
+using System;
+using System.Text;
+
+namespace Chapter01.StringCompression
+{
+    public class Solution : ISolution
+    {
+        private string[] words;
+
+        public Solution(string[] words)
+        {
+            this.words = words;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word}: Compression -> {Compression(word)}");
+            }
+        }
+
+        private string Compression(string original)
+        {
+            var sb = new StringBuilder();
+
+            int count = 1;
+            sb.Append(original[0]);
+
+            for (var i = 1; i < original.Length; i++)
+            {
+                if (original[i - 1] == original[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Append(count.ToString());
+                    sb.Append(original[i]);
+                    count = 1;
+                }
+            }
+
+            sb.Append(count);
+
+            return sb.Length < original.Length ? sb.ToString() : original;
+        }
+    }
+}
