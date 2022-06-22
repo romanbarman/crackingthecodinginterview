b6aa86b4e1831b39381a08109aca63db11fac6f4
Raman Zanko <r.zanko@godeltech.com>
2022-06-22 10:42:42 +0300

    Added graph structure

---
+++ Structures/Graph.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Structures
+{
+    public class Graph<T>
+    {
+        public class Node
+        {
+            public T Value { get; }
+            public IList<Node> Children { get; }
+            public bool IsVisited { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Children = new List<Node>();
+                IsVisited = false;
+            }
+
+            public void Add(params Node[] nodes)
+            {
+                foreach (var node in nodes)
+                {
+                    Children.Add(node);
+                }
+            }
+        }
+
+        private IList<Node> nodes = new List<Node>();
+
+        public void Add(params Node[] nodes)
+        {
+            foreach (var node in nodes)
+            {
+                this.nodes.Add(node);
+            }
+        }
+
+        public void ResetVisited()
+        {
+            foreach (var node in nodes)
+            {
+                node.IsVisited = false;
+            }
+        }
+
+        public void Show()
+        {
+            foreach (var node in nodes)
+            {
+                Console.WriteLine($"{node.Value} -> {string.Join(", ", node.Children.Select(n => n.Value))}");
+            }
+        }
+    }
+}
