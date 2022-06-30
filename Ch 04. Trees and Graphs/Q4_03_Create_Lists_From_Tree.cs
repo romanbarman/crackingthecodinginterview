543b8b603c13f1c0d20b924475dfbbf72354ac3e
Raman Zanko <r.zanko@godeltech.com>
2022-06-30 11:21:21 +0300

    Added solution for question 3 from chapter 4

---
+++ Ch 04. Trees and Graphs/Q4_03_Create_Lists_From_Tree.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Structures;
+using System.Collections.Generic;
+
+namespace Chapter04
+{
+    public class Q4_03_Create_Lists_From_Tree : BaseQuestion
+    {
+        public override string GetDescription()
+        {
+            return "For a binary tree, develop an algorithm that creates a linked list of all nodes at each depth.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            var tree = new BinaryTreeSearch<int>();
+
+            tree.Add(10);
+            tree.Add(15);
+            tree.Add(5);
+            tree.Add(17);
+            tree.Add(7);
+            tree.Add(12);
+            tree.Add(2);
+            tree.Add(19);
+            tree.Add(9);
+
+            return new ISolution[] { new CreateListsFromTree.Solution(tree) };
+        }
+    }
+}
---
+++ Ch 04. Trees and Graphs/Q4_03_Create_Lists_From_Tree/Solution.cs
@@ -0,0 +1,58 @@
+﻿using Contracts;
+using Structures;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter04.CreateListsFromTree
+{
+    public class Solution : ISolution
+    {
+        private readonly BinaryTreeSearch<int> tree;
+
+        public Solution(BinaryTreeSearch<int> tree)
+        {
+            this.tree = tree;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Binary Tree");
+            tree.Show();
+
+            var lists = new List<List<int>>();
+            CreateLists(lists, tree.Root, 0);
+
+            Console.WriteLine("Result");
+
+            for (var level = 0; level < lists.Count; level++)
+            {
+                Console.WriteLine($"Level {level + 1}: {string.Join(", ", lists[level])}");
+            }
+        }
+
+        private void CreateLists(List<List<int>> lists, BinaryTreeSearch<int>.Node node, int level)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (lists.Count < level + 1)
+            {
+                lists.Add(new List<int>());
+            }
+
+            lists[level].Add(node.Value);
+
+            CreateLists(lists, node.Left, level + 1);
+            CreateLists(lists, node.Right, level + 1);
+        }
+    }
+}
