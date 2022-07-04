fe2b3b4c2a6c7ad657ed798da6651ab5b782dbbb
Raman Zanko <r.zanko@godeltech.com>
2022-07-04 10:10:42 +0300

    Added solution for question 4 from chapter 4

---
+++ Ch 04. Trees and Graphs/Q4_04_Is_Balanced_Tree.cs
@@ -0,0 +1,56 @@
+﻿using Contracts;
+using Structures;
+using System.Collections.Generic;
+
+namespace Chapter04
+{
+    public class Q4_04_Is_Balanced_Tree : BaseQuestion
+    {
+        public override string GetDescription()
+        {
+            return "Implement a function that checks the balance of a binary tree. "
+                + "We consider a tree to be balanced if the height difference between two subtrees of any node does not exceed 1.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return new ISolution[]
+            {
+                new IsBalancedTree.Solution(new List<BinaryTreeSearch<int>> { CreateBalancedTree(), CreateNotBalancedTree() })
+            };
+        }
+
+        private static BinaryTreeSearch<int> CreateBalancedTree()
+        {
+            var tree = new BinaryTreeSearch<int>();
+
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(2);
+            tree.Add(7);
+            tree.Add(12);
+            tree.Add(17);
+            tree.Add(9);
+            tree.Add(19);
+
+            return tree;
+        }
+
+        private static BinaryTreeSearch<int> CreateNotBalancedTree()
+        {
+            var tree = new BinaryTreeSearch<int>();
+
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(7);
+            tree.Add(12);
+            tree.Add(17);
+            tree.Add(9);
+            tree.Add(19);
+
+            return tree;
+        }
+    }
+}
---
+++ Ch 04. Trees and Graphs/Q4_04_Is_Balanced_Tree/Solution.cs
@@ -0,0 +1,62 @@
+﻿using Contracts;
+using Structures;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter04.IsBalancedTree
+{
+    public class Solution : ISolution
+    {
+        private readonly IList<BinaryTreeSearch<int>> trees;
+
+        public Solution(IList<BinaryTreeSearch<int>> trees)
+        {
+            this.trees = trees;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var tree in trees)
+            {
+                Console.WriteLine();
+                tree.Show();
+                Console.WriteLine($"IsBalanced -> {IsBalanced(tree)}");
+            }
+        }
+
+        private bool IsBalanced(BinaryTreeSearch<int> tree)
+        {
+            return GetHeight(tree.Root) != int.MinValue;
+        }
+
+        private int GetHeight(BinaryTreeSearch<int>.Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var leftHeight = GetHeight(node.Left);
+            var rightHeight = GetHeight(node.Right);
+
+            if (leftHeight == int.MinValue || rightHeight == int.MinValue)
+            {
+                return int.MinValue;
+            }
+
+            if (Math.Abs(leftHeight - rightHeight) <= 1)
+            {
+                return Math.Max(leftHeight, rightHeight) + 1;
+            }
+
+            return int.MinValue;
+        }
+    }
+}
