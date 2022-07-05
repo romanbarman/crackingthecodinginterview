860d149b8ee281f574113791bf9e4a8455d260af
Raman Zanko <r.zanko@godeltech.com>
2022-07-05 10:10:08 +0300

    Added interface for Binary Tree Node

---
+++ Structures/Interfaces/IBinaryTreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace Structures.Interfaces
+{
+    public interface IBinaryTreeNode<T>
+    {
+        T Value { get; }
+        IBinaryTreeNode<T> Left { get; }
+        IBinaryTreeNode<T> Right { get; }
+        void Show();
+    }
+}
