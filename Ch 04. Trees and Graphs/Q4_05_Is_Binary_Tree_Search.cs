7fa9b4ddbdefaada2ec42ff860350677ecca735a
Raman Zanko <r.zanko@godeltech.com>
2022-07-05 10:20:47 +0300

    Added solution for question 5 from chapter 4

---
+++ Ch 04. Trees and Graphs/Q4_05_Is_Binary_Tree_Search.cs
@@ -0,0 +1,51 @@
+﻿using Contracts;
+using Structures;
+using Structures.Interfaces;
+using System.Collections.Generic;
+
+namespace Chapter04
+{
+    public class Q4_05_Is_Binary_Tree_Search : BaseQuestion
+    {
+        public override string GetDescription()
+        {
+            return "Implement a function to check if a binary tree is a binary search tree.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return new ISolution[]
+            {
+                new IsBinaryTreeSearch.Solution(new List<IBinaryTreeNode<int>> { CreateBinarySearchTree(), CreateBinaryTree() })
+            };
+        }
+
+        private static IBinaryTreeNode<int> CreateBinarySearchTree()
+        {
+            var tree = new BinaryTreeSearch<int>();
+
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(2);
+            tree.Add(7);
+            tree.Add(12);
+            tree.Add(17);
+            tree.Add(9);
+            tree.Add(19);
+
+            return tree.Root;
+        }
+
+        private static IBinaryTreeNode<int> CreateBinaryTree()
+        {
+            var tree = new BinaryTreeNode<int>(20);
+
+            tree.Left = new BinaryTreeNode<int>(10);
+            tree.Right = new BinaryTreeNode<int>(30);
+            tree.Left.Right = new BinaryTreeNode<int>(25);
+
+            return tree;
+        }
+    }
+}
---
+++ Ch 04. Trees and Graphs/Q4_05_Is_Binary_Tree_Search/Solution.cs
@@ -0,0 +1,59 @@
+﻿using Contracts;
+using Structures.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter04.IsBinaryTreeSearch
+{
+    public class Solution : ISolution
+    {
+        private readonly IList<IBinaryTreeNode<int>> trees;
+
+        public Solution(IList<IBinaryTreeNode<int>> trees)
+        {
+            this.trees = trees;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var tree in trees)
+            {
+                Console.WriteLine();
+                tree.Show();
+                Console.WriteLine($"IsBinaryTreeSearch -> {IsBinaryTreeSearch(tree)}");
+            }
+        }
+
+        private bool IsBinaryTreeSearch(IBinaryTreeNode<int> tree)
+        {
+            return IsBinaryTreeSearch(tree, null, null);
+        }
+
+        private bool IsBinaryTreeSearch(IBinaryTreeNode<int> tree, int? min, int? max)
+        {
+            if (tree == null)
+            {
+                return true;
+            }
+
+            if ((min.HasValue && tree.Value <= min) || (max.HasValue && tree.Value > max))
+            {
+                return false;
+            }
+
+            if (!IsBinaryTreeSearch(tree.Left, min, tree.Value) || !IsBinaryTreeSearch(tree.Right, tree.Value, max))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
