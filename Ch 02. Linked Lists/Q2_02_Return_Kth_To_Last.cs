b58d44592016531d0acf7de237aff0078ccde36e
Raman Zanko <r.zanko@godeltech.com>
2022-07-08 15:41:36 +0300

    Updated architecture for question 2 from chapter 2

--- Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last.cs
+++ Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last.cs
@@ -1,49 +1,23 @@
 ﻿using Contracts;
-using Structures;
-using System;
+using System.Collections.Generic;
 
 namespace Chapter02
 {
-    public class Q2_02_Return_Kth_To_Last : IQuestion
+    public class Q2_02_Return_Kth_To_Last : BaseQuestion
     {
-        public string GetDescription()
+        private static readonly ISolution[] solutins = new ISolution[]
+        {
+            new ReturnKthToLast.Solution(Structures.LinkedListNode<int>.CreateLinkedList(0, 20, 20), 19)
+        };
+
+        public override string GetDescription()
         {
             return "Implement an algorithm to find the kth element from the end in a singly linked list.";
         }
 
-        public void Run()
+        protected override IList<ISolution> GetSolutions()
         {
-            var linkedList = LinkedListNode<int>.CreateLinkedList(0, 20, 20);
-            var k = 19;
-            var node = FindKToLast(linkedList, k);
-
-            linkedList.Show();
-            Console.Write($": FindKToLast {k} -> {node.Value}");
-            Console.WriteLine();
-        }
-
-        private LinkedListNode<int> FindKToLast(LinkedListNode<int> top, int k)
-        {
-            var first = top;
-            var second = top;
-
-            for (var i = 0; i < k; i++)
-            {
-                if (second == null)
-                {
-                    return null;
-                }
-
-                second = second.Next;
-            }
-
-            while (second != null)
-            {
-                first = first.Next;
-                second = second.Next;
-            }
-
-            return first;
+            return solutins;
         }
     }
 }
---
+++ Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/Solution.cs
@@ -0,0 +1,56 @@
+﻿using Contracts;
+using Structures;
+using System;
+
+namespace Chapter02.ReturnKthToLast
+{
+    public class Solution : ISolution
+    {
+        private readonly LinkedListNode<int> top;
+        private readonly int k;
+
+        public Solution(LinkedListNode<int> top, int k)
+        {
+            this.top = top;
+            this.k = k;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            top.Show();
+            Console.Write($": FindKToLast {k} -> {FindKToLast().Value}");
+            Console.WriteLine();
+        }
+
+        private LinkedListNode<int> FindKToLast()
+        {
+            var first = top;
+            var second = top;
+
+            for (var i = 0; i < k; i++)
+            {
+                if (second == null)
+                {
+                    return null;
+                }
+
+                second = second.Next;
+            }
+
+            while (second != null)
+            {
+                first = first.Next;
+                second = second.Next;
+            }
+
+            return first;
+        }
+    }
+}
