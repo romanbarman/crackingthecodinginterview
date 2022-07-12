6e0804653c5837fc4bb5f386573025b649f1795c
Raman Zanko <r.zanko@godeltech.com>
2022-07-12 16:57:25 +0300

    Added solutions for question 6 from chapter 4

---
+++ Ch 04. Trees and Graphs/Q4_06_Find_Next_Node,.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using Structures;
+using Structures.Interfaces;
+using System.Collections.Generic;
+
+namespace Chapter04
+{
+    public class Q4_06_Find_Next_Node : BaseQuestion
+    {
+        public override string GetDescription()
+        {
+            return "Write an algorithm for finding the \"next\" node for the posterior node in the binary search tree. "
+                + "Consider that each node has a link to its parent.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            var tree = CreateTree();
+            return new ISolution[] { new FindNextNode.Solution(tree, tree.Right.Right) };
+        }
+
+        private IBinaryTreeNode<int> CreateTree()
+        {
+            var tree = new BinaryTreeSearch<int>();
+
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(2);
+            tree.Add(7);
+            tree.Add(12);
+            tree.Add(17);
+            tree.Add(9);
+            tree.Add(19);
+
+            return tree.Root;
+        }
+    }
+}
---
+++ Ch 04. Trees and Graphs/Q4_06_Find_Next_Node/Solution.cs
@@ -0,0 +1,67 @@
+﻿using Contracts;
+using Structures.Interfaces;
+using System;
+
+namespace Chapter04.FindNextNode
+{
+    public class Solution : ISolution
+    {
+        private readonly IBinaryTreeNode<int> tree;
+        private readonly IBinaryTreeNode<int> node;
+
+        public Solution(IBinaryTreeNode<int> tree, IBinaryTreeNode<int> node)
+        {
+            this.tree = tree;
+            this.node = node;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            tree.Show();
+            Console.WriteLine($"InorderSucc({node.Value}) -> {InorderSucc()?.Value}");
+        }
+
+        private IBinaryTreeNode<int> InorderSucc()
+        {
+            if (node.Right != null)
+            {
+                return LeftMostChild(node.Right);
+            }
+            else
+            {
+                var q = node;
+                var x = q.Parent;
+
+                while (x != null && x.Left != q)
+                {
+                    q = x;
+                    x = x.Parent;
+                }
+
+                return x;
+            }
+        }
+
+        private IBinaryTreeNode<int> LeftMostChild(IBinaryTreeNode<int> n)
+        {
+            if (n == null)
+            {
+                return null;
+            }
+
+            while (n.Left != null)
+            {
+                n = n.Left;
+            }
+
+            return n;
+        }
+    }
+}
