059d53ddeec9a12ad1bca2b8a088e9f195737156
Raman Zanko <r.zanko@godeltech.com>
2022-07-12 10:34:40 +0300

    Updated architecture for question 4 from chapter 2

--- Ch 02. Linked Lists/Q2_04_Partition.cs
+++ Ch 02. Linked Lists/Q2_04_Partition.cs
@@ -1,97 +1,25 @@
 ﻿using Contracts;
-using Structures;
-using System;
+using System.Collections.Generic;
 
 namespace Chapter02
 {
-    public class Q2_04_Partition : IQuestion
+    public class Q2_04_Partition : BaseQuestion
     {
-        public string GetDescription()
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new Partition.Solution(Structures.LinkedListNode<int>.CreateLinkedList(new[] { 3, 5, 8, 5, 10, 2, 1 }))
+        };
+
+        public override string GetDescription()
         {
             return "Write code to split a linked list around x so that all nodes less than x precede nodes greater than or equal to x. "
                 + "If x is contained in a list, then the values of x must follow strictly after elements less than x. "
                 + "Breakdown element x can be anywhere in the right part; it does not have to be located between the left and right parts.";
         }
 
-        public void Run()
+        protected override IList<ISolution> GetSolutions()
         {
-            var linkedList = LinkedListNode<int>.CreateLinkedList(new[] { 3, 5, 8, 5, 10, 2, 1 });
-            linkedList.Show();
-            var value = 5;
-            Console.Write($": Partition {value} -> ");
-            Partition(ref linkedList, value);
-            linkedList.Show();
-            Console.WriteLine();
-        }
-
-        private void Partition(ref LinkedListNode<int> top, int value)
-        {
-            LinkedListNode<int> beforeMore;
-
-            if (!GetBeforeLessElement(top, value, out beforeMore))
-            {
-                return;
-            }
-
-            LinkedListNode<int> currentSmall = null;
-
-            var current = top;
-            if (beforeMore == null)
-            {
-                currentSmall = top;
-                current = top.Next;
-            }
-            else
-            {
-                currentSmall = beforeMore.Next;
-                beforeMore.SetNext(beforeMore.Next.Next);
-            }
-
-            currentSmall.SetNext(null);
-            var newTop = currentSmall;
-
-
-            while (current != null)
-            {
-                var next = current.Next;
-                current.SetNext(currentSmall.Next);
-                currentSmall.SetNext(current);
-
-                if (current.Value < value)
-                {
-                    currentSmall = currentSmall.Next;
-                }
-
-                current = next;
-            }
-
-            top = newTop;
-        }
-
-        private bool GetBeforeLessElement(LinkedListNode<int> top, int value, out LinkedListNode<int> node)
-        {
-            var current = top;
-
-            if (current.Value < value)
-            {
-                node = null;
-                return true;
-            }
-
-            while (current.Next != null)
-            {
-
-                if (current.Next.Value < value)
-                {
-                    node = current;
-                    return true;
-                }
-
-                current = current.Next;
-            }
-
-            node = null;
-            return false; ;
+            return solutions;
         }
     }
 }
---
+++ Ch 02. Linked Lists/Q2_04_Partition/Solution.cs
@@ -0,0 +1,102 @@
+﻿using Contracts;
+using Structures;
+using System;
+
+namespace Chapter02.Partition
+{
+    public class Solution : ISolution
+    {
+        private LinkedListNode<int> linkedList;
+
+        public Solution(LinkedListNode<int> linkedList)
+        {
+            this.linkedList = linkedList;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            linkedList.Show();
+            var value = 5;
+            Console.Write($": Partition {value} -> ");
+            Partition(ref linkedList, value);
+            linkedList.Show();
+        }
+
+        private void Partition(ref LinkedListNode<int> top, int value)
+        {
+            LinkedListNode<int> beforeMore;
+
+            if (!GetBeforeLessElement(top, value, out beforeMore))
+            {
+                return;
+            }
+
+            LinkedListNode<int> currentSmall = null;
+
+            var current = top;
+            if (beforeMore == null)
+            {
+                currentSmall = top;
+                current = top.Next;
+            }
+            else
+            {
+                currentSmall = beforeMore.Next;
+                beforeMore.SetNext(beforeMore.Next.Next);
+            }
+
+            currentSmall.SetNext(null);
+            var newTop = currentSmall;
+
+
+            while (current != null)
+            {
+                var next = current.Next;
+                current.SetNext(currentSmall.Next);
+                currentSmall.SetNext(current);
+
+                if (current.Value < value)
+                {
+                    currentSmall = currentSmall.Next;
+                }
+
+                current = next;
+            }
+
+            top = newTop;
+        }
+
+        private bool GetBeforeLessElement(LinkedListNode<int> top, int value, out LinkedListNode<int> node)
+        {
+            var current = top;
+
+            if (current.Value < value)
+            {
+                node = null;
+                return true;
+            }
+
+            while (current.Next != null)
+            {
+
+                if (current.Next.Value < value)
+                {
+                    node = current;
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            node = null;
+            return false; ;
+        }
+    }
+}
