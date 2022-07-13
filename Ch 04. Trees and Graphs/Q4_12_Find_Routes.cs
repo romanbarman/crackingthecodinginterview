837bbcade51455216447422ae28430a12dd6a76c
Raman Zanko <r.zanko@godeltech.com>
2022-07-13 12:11:59 +0300

    Added solutions for question 12 from chapter 4

---
+++ Ch 04. Trees and Graphs/Q4_12_Find_Routes.cs
@@ -0,0 +1,44 @@
+﻿using Contracts;
+using Structures;
+using Structures.Interfaces;
+using System.Collections.Generic;
+
+namespace Chapter04
+{
+    public class Q4_12_Find_Routes : BaseQuestion
+    {
+        public override string GetDescription()
+        {
+            return "Given a binary tree in which each node contains an integer. "
+                + "Develop an algorithm to count all paths whose sum equals a given value. "
+                + "Note that the path does not have to start or end at the root or leaf node, but must go down.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return new ISolution[] { new FindRoutes.Solution(CreateTree(), 8) };
+        }
+
+        private IBinaryTreeNode<int> CreateTree()
+        {
+            var binaryTreeNode = new BinaryTreeNode<int>(10);
+
+            binaryTreeNode.SetLeft(new BinaryTreeNode<int>(5));
+            binaryTreeNode.SetRight(new BinaryTreeNode<int>(-3));
+
+            binaryTreeNode.Left.SetLeft(new BinaryTreeNode<int>(3));
+            binaryTreeNode.Left.SetRight(new BinaryTreeNode<int>(1));
+
+            binaryTreeNode.Right.SetRight(new BinaryTreeNode<int>(11));
+
+            binaryTreeNode.Left.Left.SetLeft(new BinaryTreeNode<int>(3));
+            binaryTreeNode.Left.Left.SetRight(new BinaryTreeNode<int>(-2));
+
+            binaryTreeNode.Left.Right.SetRight(new BinaryTreeNode<int>(2));
+
+            binaryTreeNode.Left.Left.Right.SetRight(new BinaryTreeNode<int>(2));
+
+            return binaryTreeNode;
+        }
+    }
+}
---
+++ Ch 04. Trees and Graphs/Q4_12_Find_Routes/Solution.cs
@@ -0,0 +1,67 @@
+﻿using Contracts;
+using Structures.Interfaces;
+using System;
+
+namespace Chapter04.FindRoutes
+{
+    public class Solution : ISolution
+    {
+        private readonly IBinaryTreeNode<int> tree;
+        private readonly int sum;
+
+        public Solution(IBinaryTreeNode<int> tree, int sum)
+        {
+            this.tree = tree;
+            this.sum = sum;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Sum: {sum}");
+            tree.Show();
+            Console.WriteLine($"CountPath -> {CountPath(tree)}");
+        }
+
+        private int CountPath(IBinaryTreeNode<int> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var pathsFromRoot = CountPathsWithSumFromNode(root, 0);
+            var pathsOnLeft = CountPath(root.Left);
+            var pathsOnRight = CountPath(root.Right);
+
+            return pathsFromRoot + pathsOnLeft + pathsOnRight;
+        }
+
+        private int CountPathsWithSumFromNode(IBinaryTreeNode<int> node, int currentSum)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            currentSum += node.Value;
+            var totalPaths = 0;
+
+            if (currentSum == sum)
+            {
+                totalPaths++;
+            }
+
+            totalPaths += CountPathsWithSumFromNode(node.Left, currentSum);
+            totalPaths += CountPathsWithSumFromNode(node.Right, currentSum);
+
+            return totalPaths;
+        }
+    }
+}
