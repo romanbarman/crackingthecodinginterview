e4f9846370d48d6bda25ca5b9102eb1d6f49e652
Raman Zanko <r.zanko@godeltech.com>
2022-07-15 10:42:57 +0300

    Added solutions for question 2 from chapter 5

---
+++ Ch 05. Bit Operations/Q5_02_Double_To_Binary.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter05
+{
+    public class Q5_02_Double_To_Binary : BaseQuestion
+    {
+        private readonly ISolution[] solutions = new ISolution[]
+        {
+            new DoubleToBinary.Solution(new [] { 0.25, 0.72 })
+        };
+
+        public override string GetDescription()
+        {
+            return "You are given a real number between 0 and 1, which is transmitted in double format. "
+                + "Output its binary representation. "
+                + "If the exact binary representation of a number is not 32 bits long, print an error message";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 05. Bit Operations/Q5_02_Double_To_Binary/Solution.cs
@@ -0,0 +1,61 @@
+﻿using Contracts;
+using System;
+using System.Text;
+
+namespace Chapter05.DoubleToBinary
+{
+    public class Solution : ISolution
+    {
+        private readonly double[] numbers;
+
+        public Solution(double[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"Number = {number}");
+                Console.WriteLine($"ToBinary -> {ToBinary(number)}");
+                Console.WriteLine();
+            }
+        }
+
+        public string ToBinary(double number)
+        {
+            var sb = new StringBuilder();
+            sb.Append(".");
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (number == 0)
+                {
+                    return sb.ToString();
+                }
+
+                number = number * 2;
+
+                if (number >= 1)
+                {
+                    sb.Append("1");
+                    number = number - 1;
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+            }
+
+            return "Error";
+        }
+    }
+}
