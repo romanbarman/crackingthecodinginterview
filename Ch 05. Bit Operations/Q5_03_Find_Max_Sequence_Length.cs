64755912b989cd31d39fe057cb2cf45a0110efa6
Raman Zanko <r.zanko@godeltech.com>
2022-07-18 09:34:50 +0300

    Added solutions for question 3 from chapter 5

---
+++ Ch 05. Bit Operations/Q5_03_Find_Max_Sequence_Length.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter05
+{
+    public class Q5_03_Find_Max_Sequence_Length : BaseQuestion
+    {
+        private static readonly ISolution[] solutions = new ISolution[]
+        {
+            new FindMaxSequenceLength.Solution(0b11011101111)
+        };
+
+        public override string GetDescription()
+        {
+            return "There is an integer in which exactly one bit can be changed from 0 to 1. "
+                + "Write code to find the length of the longest sequence of 1's that can be obtained.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 05. Bit Operations/Q5_03_Find_Max_Sequence_Length/Solution.cs
@@ -0,0 +1,57 @@
+﻿using Contracts;
+using System;
+
+namespace Chapter05.FindMaxSequenceLength
+{
+    public class Solution : ISolution
+    {
+        private int number;
+
+        public Solution(int number)
+        {
+            this.number = number;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Number: {Convert.ToString(number, 2)}");
+            Console.WriteLine($"FindMaxLength -> {FindMaxLength()}");
+        }
+
+        private int FindMaxLength()
+        {
+            if (~number == 0)
+            {
+                return 32;
+            }
+
+            var currentLength = 0;
+            var previosLength = 0;
+            int maxLength = 0;
+
+            while (number != 0)
+            {
+                if ((number & 1) == 1)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    previosLength = (number & 2) == 0 ? 0 : currentLength;
+                    currentLength = 0;
+                }
+                maxLength = Math.Max(previosLength + currentLength + 1, maxLength);
+                number = (int)((uint)number >> 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
