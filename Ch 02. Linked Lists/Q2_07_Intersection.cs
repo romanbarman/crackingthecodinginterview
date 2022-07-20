39577a3460a4d30bf10a2adc85204d1261df1efc
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 10:00:53 +0300

    Updated architecture for question 7 from chapter 2

--- Ch 02. Linked Lists/Q2_07_Intersection.cs
+++ Ch 02. Linked Lists/Q2_07_Intersection.cs
@@ -1,121 +1,38 @@
 ﻿using Contracts;
-using Structures;
-using System;
+using System.Collections.Generic;
 
 namespace Chapter02
 {
-    public class Q2_07_Intersection : IQuestion
+    public class Q2_07_Intersection : BaseQuestion
     {
-        public string GetDescription()
+        public override string GetDescription()
         {
             return "Check if two given singly linked lists intersect. Return the intersection node.";
         }
 
-        public void Run()
+        protected override IList<ISolution> GetSolutions()
         {
-            var list1 = LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4 });
-            var list2 = LinkedListNode<int>.CreateLinkedList(new[] { 5, 6 });
-            var list3 = LinkedListNode<int>.CreateLinkedList(new[] { 7, 8, 9 });
+            return new ISolution[] { new Intersection.Solution(new[] { CreateWithIntersection(), CreateWithoutIntersection() }) };
+        }
+
+        private static (Structures.LinkedListNode<int>, Structures.LinkedListNode<int>) CreateWithIntersection()
+        {
+            var list1 = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4 });
+            var list2 = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 5, 6 });
+            var list3 = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 7, 8, 9 });
 
             list1.Next.Next.Next.SetNext(list3);
             list2.Next.SetNext(list3);
 
-            Show(list1, list2);
-
-            list1 = LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4 });
-            list2 = LinkedListNode<int>.CreateLinkedList(new[] { 5, 6 });
-
-            Show(list1, list2);
+            return (list1, list2);
         }
 
-        private void Show(LinkedListNode<int> list1, LinkedListNode<int> list2)
+        private static (Structures.LinkedListNode<int>, Structures.LinkedListNode<int>) CreateWithoutIntersection()
         {
-            list1.Show();
-            Console.Write("; ");
-            list2.Show();
-            Console.WriteLine($": GetIntersectNode -> {GetIntersectNode(list1, list2)?.Value.ToString() ?? "NO"}");
-            Console.WriteLine();
-        }
+            var list1 = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4 });
+            var list2 = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 5, 6 });
 
-        private LinkedListNode<int> GetIntersectNode(LinkedListNode<int> list1, LinkedListNode<int> list2)
-        {
-            if (!HasIntersection(list1, list2))
-            {
-                return null;
-            }
-
-            var list1Length = GetLength(list1);
-            var list2Length = GetLength(list2);
-
-            if (list1Length == list2Length)
-            {
-                return GetIntersect(list1, list2);
-            }
-
-            if (list1Length > list2Length)
-            {
-                return GetIntersect(Move(list1, list1Length - list2Length), list2);
-            }
-
-            return GetIntersect(list1, Move(list2, list1Length - list2Length));
-        }
-
-        private LinkedListNode<int> Move(LinkedListNode<int> node, int steps)
-        {
-            for (int i = 0; i < steps; i++)
-            {
-                node = node.Next;
-            }
-
-            return node;
-        }
-
-
-        private LinkedListNode<int> GetIntersect(LinkedListNode<int> node1, LinkedListNode<int> node2)
-        {
-            while (node1 != null && node2 != null)
-            {
-                if (node1 == node2)
-                {
-                    return node1;
-                }
-
-                node1 = node1.Next;
-                node2 = node2.Next;
-            }
-
-            return null;
-        }
-
-        private int GetLength(LinkedListNode<int> top)
-        {
-            var current = top;
-            int length = 1;
-
-            while (current.Next != null)
-            {
-                current = current.Next;
-                length++;
-            }
-
-            return length;
-        }
-
-        private bool HasIntersection(LinkedListNode<int> list1, LinkedListNode<int> list2)
-        {
-            return GetLastElement(list1) == GetLastElement(list2);
-        }
-
-        private LinkedListNode<int> GetLastElement(LinkedListNode<int> top)
-        {
-            var current = top;
-
-            while (current.Next != null)
-            {
-                current = current.Next;
-            }
-
-            return current;
+            return (list1, list2);
         }
     }
 }
---
+++ Ch 02. Linked Lists/Q2_07_Intersection/Solution.cs
@@ -0,0 +1,121 @@
+﻿using Contracts;
+using Structures;
+using System;
+
+namespace Chapter02.Intersection
+{
+    public class Solution : ISolution
+    {
+        private (LinkedListNode<int> list1, LinkedListNode<int> list2)[] listsPairs;
+
+        public Solution((LinkedListNode<int>, LinkedListNode<int>)[] listsPairs)
+        {
+            this.listsPairs = listsPairs;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var pair in listsPairs)
+            {
+                Show(pair.list1, pair.list2);
+            }
+        }
+
+        private static void Show(LinkedListNode<int> list1, LinkedListNode<int> list2)
+        {
+            list1.Show();
+            Console.Write("; ");
+            list2.Show();
+            Console.WriteLine($": GetIntersectNode -> {GetIntersectNode(list1, list2)?.Value.ToString() ?? "NO"}");
+            Console.WriteLine();
+        }
+
+        private static LinkedListNode<int> GetIntersectNode(LinkedListNode<int> list1, LinkedListNode<int> list2)
+        {
+            if (!HasIntersection(list1, list2))
+            {
+                return null;
+            }
+
+            var list1Length = GetLength(list1);
+            var list2Length = GetLength(list2);
+
+            if (list1Length == list2Length)
+            {
+                return GetIntersect(list1, list2);
+            }
+
+            if (list1Length > list2Length)
+            {
+                return GetIntersect(Move(list1, list1Length - list2Length), list2);
+            }
+
+            return GetIntersect(list1, Move(list2, list1Length - list2Length));
+        }
+
+        private static LinkedListNode<int> Move(LinkedListNode<int> node, int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                node = node.Next;
+            }
+
+            return node;
+        }
+
+
+        private static LinkedListNode<int> GetIntersect(LinkedListNode<int> node1, LinkedListNode<int> node2)
+        {
+            while (node1 != null && node2 != null)
+            {
+                if (node1 == node2)
+                {
+                    return node1;
+                }
+
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+
+            return null;
+        }
+
+        private static int GetLength(LinkedListNode<int> top)
+        {
+            var current = top;
+            int length = 1;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+                length++;
+            }
+
+            return length;
+        }
+
+        private static bool HasIntersection(LinkedListNode<int> list1, LinkedListNode<int> list2)
+        {
+            return GetLastElement(list1) == GetLastElement(list2);
+        }
+
+        private static LinkedListNode<int> GetLastElement(LinkedListNode<int> top)
+        {
+            var current = top;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+    }
+}
