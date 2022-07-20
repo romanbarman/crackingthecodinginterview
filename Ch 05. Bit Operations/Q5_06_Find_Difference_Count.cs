e18ba2310ef8273f54002bcd905f170fca9b4cbb
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 10:30:46 +0300

    Added solutions for question 6 from chapter 5

---
+++ Ch 05. Bit Operations/Q5_06_Find_Difference_Count.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter05
+{
+    public class Q5_06_Find_Difference_Count : BaseQuestion
+    {
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new FindDifferenceCount.Solution(29, 15)
+        };
+
+        public override string GetDescription()
+        {
+            return "Write a function that determines the number of bits that need to be changed "
+                + "in order to get an integer B from an integer A.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 05. Bit Operations/Q5_06_Find_Difference_Count/Solution.cs
@@ -0,0 +1,42 @@
+﻿using Contracts;
+using System;
+
+namespace Chapter05.FindDifferenceCount
+{
+    public class Solution : ISolution
+    {
+        private int a;
+        private int b;
+
+        public Solution(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"A: {a} -> {Convert.ToString(a, 2)}");
+            Console.WriteLine($"B: {b} -> {Convert.ToString(b, 2)}");
+            Console.WriteLine($"CountBitsDif -> {CountBitsDif()}");
+        }
+
+        private int CountBitsDif()
+        {
+            var count = 0;
+            for (var i = a ^ b; i != 0 ; i = i >> 1)
+            {
+                count += i & 1;
+            }
+
+            return count;
+        }
+    }
+}
