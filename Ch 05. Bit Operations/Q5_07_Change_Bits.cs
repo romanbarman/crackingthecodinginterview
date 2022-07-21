0206c5677589059e0b678e2b0f4150114c8a6f27
Raman Zanko <r.zanko@godeltech.com>
2022-07-21 09:31:48 +0300

    Added solutions for question 7 from chapter 5

---
+++ Ch 05. Bit Operations/Q5_07_Change_Bits.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter05
+{
+    public class Q5_07_Change_Bits : BaseQuestion
+    {
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new ChangeBits.Solution(0b1001001)
+        };
+
+        public override string GetDescription()
+        {
+            return "Write a program that swaps even and odd bits of a number with as few instructions as possible.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 05. Bit Operations/Q5_07_Change_Bits/Solution.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using System;
+
+namespace Chapter05.ChangeBits
+{
+    public class Solution : ISolution
+    {
+        private int number;
+
+        public Solution(int number) => this.number = number;
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Number: {number} -> {Convert.ToString(number, 2)}");
+            Console.WriteLine($"SwapOddEvenBits -> {Convert.ToString(SwapOddEvenBits(number), 2)}");
+        }
+
+        private static int SwapOddEvenBits(int x)
+        {
+            return (int)((uint) (x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1);
+        }
+    }
+}
