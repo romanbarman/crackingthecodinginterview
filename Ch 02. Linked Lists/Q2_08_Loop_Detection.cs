c666eaddd7633b166bb5674c915259105aba4fa8
Raman Zanko <r.zanko@godeltech.com>
2022-07-21 09:46:42 +0300

    Updated architecture for question 8 from chapter 2

--- Ch 02. Linked Lists/Q2_08_Loop_Detection.cs
+++ Ch 02. Linked Lists/Q2_08_Loop_Detection.cs
@@ -1,99 +1,31 @@
 ﻿using Contracts;
-using Structures;
-using System;
+using System.Collections.Generic;
 
 namespace Chapter02
 {
-    public class Q2_08_Loop_Detection : IQuestion
+    public class Q2_08_Loop_Detection : BaseQuestion
     {
-        public string GetDescription()
+        public override string GetDescription()
         {
             return "For a circular linked list, implement an algorithm that returns the starting node of the loop.";
         }
 
-        public void Run()
+        protected override IList<ISolution> GetSolutions()
         {
-            var list1 = LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4, 5, 6 , 7 , 8 , 9 , 10 });
-            list1.Next.Next.Next.Next.Next.Next.Next.Next.Next.SetNext(list1.Next.Next);
-
-            ShowLoopList(list1, 11);
-            Console.WriteLine($": GetLoopBegin -> {GetLoopBegin(list1)?.Value.ToString() ?? "NO"}");
-            Console.WriteLine();
-
-            var list2 = LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-            list2.Show();
-            Console.WriteLine($": GetLoopBegin -> {GetLoopBegin(list2)?.Value.ToString() ?? "NO"}");
-            Console.WriteLine();
+            return new ISolution[] { new LoopDetection.Solution(new[] { CreateLoopList(), CreateNotLoopList() }) };
         }
 
-        private void ShowLoopList(LinkedListNode<int> top, int nodesCount)
+        private static (Structures.LinkedListNode<int>, int) CreateNotLoopList()
         {
-            var current = top;
-
-            for (int i = 0; i < nodesCount; i++)
-            {
-                Console.Write($"{current.Value} ");
-                current = current.Next;
-            }
+            return (Structures.LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }), 10);
         }
 
-        private LinkedListNode<int> GetLoopBegin(LinkedListNode<int> top)
+        private static (Structures.LinkedListNode<int>, int) CreateLoopList()
         {
-            if (!IsLoop(top))
-            {
-                return null;
-            }
+            var list = Structures.LinkedListNode<int>.CreateLinkedList(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            list.Next.Next.Next.Next.Next.Next.Next.Next.Next.SetNext(list.Next.Next);
 
-            var slow = top;
-            var fast = top;
-
-            while (true)
-            {
-                fast = fast.Next.Next;
-                slow = slow.Next;
-
-                if (fast == slow)
-                {
-                    break;
-                }
-            }
-
-            slow = top;
-
-            while (true)
-            {
-                if (slow == fast)
-                {
-                    return slow;
-                }
-
-                slow = slow.Next;
-                fast = fast.Next;
-            }
-        }
-
-        private bool IsLoop(LinkedListNode<int> top)
-        {
-            var slow = top;
-            var fast = top;
-
-            while (fast != null)
-            {
-                if (fast.Next?.Next == null)
-                {
-                    return false;
-                }
-
-                fast = fast.Next.Next;
-                slow = slow.Next;
-
-                if (fast == slow)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return (list, 11);
         }
     }
 }
---
+++ Ch 02. Linked Lists/Q2_08_Loop_Detection/Solution.cs
@@ -0,0 +1,100 @@
+﻿using Contracts;
+using Structures;
+using System;
+
+namespace Chapter02.LoopDetection
+{
+    public class Solution : ISolution
+    {
+        private readonly (LinkedListNode<int> list, int length)[] listsInfo;
+
+        public Solution((LinkedListNode<int>, int)[] listsInfo) => this.listsInfo = listsInfo;
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            foreach (var info in listsInfo)
+            {
+                ShowList(info.list, info.length);
+                Console.WriteLine($": GetLoopBegin -> {GetLoopBegin(info.list)?.Value.ToString() ?? "NO"}");
+                Console.WriteLine();
+            }
+        }
+
+        private static LinkedListNode<int> GetLoopBegin(LinkedListNode<int> top)
+        {
+            if (!IsLoop(top))
+            {
+                return null;
+            }
+
+            var slow = top;
+            var fast = top;
+
+            while (true)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+
+                if (fast == slow)
+                {
+                    break;
+                }
+            }
+
+            slow = top;
+
+            while (true)
+            {
+                if (slow == fast)
+                {
+                    return slow;
+                }
+
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+        }
+
+        private static bool IsLoop(LinkedListNode<int> top)
+        {
+            var slow = top;
+            var fast = top;
+
+            while (fast != null)
+            {
+                if (fast.Next?.Next == null)
+                {
+                    return false;
+                }
+
+                fast = fast.Next.Next;
+                slow = slow.Next;
+
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void ShowList(LinkedListNode<int> top, int nodesCount)
+        {
+            var current = top;
+
+            for (int i = 0; i < nodesCount; i++)
+            {
+                Console.Write($"{current.Value} ");
+                current = current.Next;
+            }
+        }
+    }
+}
