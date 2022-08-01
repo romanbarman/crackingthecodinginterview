9b2384d595019d065e6ad25600eb9a1ad5a73081
Raman Zanko <r.zanko@godeltech.com>
2022-08-01 10:11:58 +0300

    Added solutions for question 1 from chapter 8

---
+++ Ch 08. Recursion And Dynamic Programming/Q8_01_Stairs.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter08
+{
+    public class Q8_01_Stairs : BaseQuestion
+    {
+        private static readonly ISolution[] solutions = new ISolution[]
+        {
+            new Stairs.Solution(10)
+        };
+
+        public override string GetDescription()
+        {
+            return "The child climbs the stairs from n steps. "
+                + "In one step, he can move one, two or three steps. "
+                + "Implement a method that calculates the number of possible ways a child can move up the stairs.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
