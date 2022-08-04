44fa7b9db0e279a199f5830635ca8e4712fa3651
Raman Zanko <r.zanko@godeltech.com>
2022-08-04 15:08:35 +0300

    Updated architecture for question 4 from chapter 3

---
+++ Ch 03. Stack and Queue/Q3_04_Queue_With_Stacks/MyQueue.cs
@@ -0,0 +1,72 @@
+﻿using Structures;
+using System;
+
+namespace Chapter03.QueueWithStacks
+{
+    public class MyQueue
+    {
+        private Stack<int> stack = new Stack<int>();
+        private Stack<int> queue = new Stack<int>();
+        private bool isAdd = false;
+        private bool isRemove = false;
+
+        public void Add(int value)
+        {
+            if (isRemove)
+            {
+                Move(stack, queue);
+                isRemove = false;
+            }
+
+            stack.Push(value);
+            isAdd = true;
+        }
+
+        public int Remove()
+        {
+            if (isAdd)
+            {
+                Move(queue, stack);
+                isAdd = false;
+            }
+
+            if (queue.IsEmpty())
+            {
+                throw new InvalidOperationException();
+            }
+
+            isRemove = true;
+            return queue.Pop();
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.IsEmpty() && queue.IsEmpty();
+        }
+
+        public int Peek()
+        {
+            if (isAdd)
+            {
+                Move(queue, stack);
+                isAdd = false;
+            }
+
+            if (queue.IsEmpty())
+            {
+                throw new InvalidOperationException();
+            }
+
+            isRemove = true;
+            return queue.Peek();
+        }
+
+        private void Move(Stack<int> destination, Stack<int> source)
+        {
+            while (!source.IsEmpty())
+            {
+                destination.Push(source.Pop());
+            }
+        }
+    }
+}
