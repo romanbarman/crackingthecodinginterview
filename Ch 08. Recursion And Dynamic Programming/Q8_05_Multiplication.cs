20c8e0c50286a63ca2b5655e34238c9b250af745
Raman Zanko <r.zanko@godeltech.com>
2022-08-04 14:58:04 +0300

    Added solutions for question 5 from chapter 8

---
+++ Ch 08. Recursion And Dynamic Programming/Q8_05_Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter08
+{
+    public class Q8_05_Multiplication : BaseQuestion
+    {
+        private static readonly ISolution[] solutions = new ISolution[]
+        {
+            new Multiplication.Solution(11, 7)
+        };
+
+        public override string GetDescription()
+        {
+            return "Write a recursive function to multiply two positive integers without using the * operator. "
+                + "It is allowed to use addition, subtraction and bitwise shift operations, but their number should be minimal.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
