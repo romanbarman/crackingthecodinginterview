61693fe5c628902cd268a5484a61bfc4870bb63d
Raman Zanko <r.zanko@godeltech.com>
2022-08-12 09:52:11 +0300

    Added solutions for question 7 from chapter 8

---
+++ Ch 08. Recursion And Dynamic Programming/Q8_07_String_Permutations.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter08
+{
+    public class Q8_07_String_Permutations : BaseQuestion
+    {
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new StringPermutations.Solution("abcd")
+        };
+
+        public override string GetDescription()
+        {
+            return "Write a method to calculate all permutations of a string consisting of unique characters.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 08. Recursion And Dynamic Programming/Q8_07_String_Permutations/Solution.cs
@@ -0,0 +1,68 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Chapter08.StringPermutations
+{
+    public class Solution : ISolution
+    {
+        private string str;
+
+        public Solution(string str)
+        {
+            this.str = str;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(str);
+            Console.WriteLine();
+            Console.WriteLine("GetAllPermutations:");
+
+            foreach (var permutation in GetAllPermutations())
+            {
+                Console.WriteLine(permutation);
+            }
+        }
+
+        private List<string> GetAllPermutations()
+        {
+            var list = new List<string>();
+            list.Add(str);
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                var newPermutations = new List<string>();
+
+                foreach (var s in list)
+                {
+                    for (int j = i + 1; j < str.Length; j++)
+                    {
+                        newPermutations.Add(Swap(s, i, j));
+                    }
+                }
+
+                list.AddRange(newPermutations);
+            }
+
+            return list;
+        }
+
+        private static string Swap(string str, int i, int j)
+        {
+            var chars = str.ToCharArray();
+            var temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+
+            return new string(chars);
+        }
+    }
+}
