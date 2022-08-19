7a4633e59aed21179c7a4bff86561ae150275d46
Raman Zanko <r.zanko@godeltech.com>
2022-08-19 10:35:27 +0300

    Added solutions for question 9 from chapter 8

---
+++ Ch 08. Recursion And Dynamic Programming/Q8_09_Parentheses_Combination.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter08
+{
+    public class Q8_09_Parentheses_Combination : BaseQuestion
+    {
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new ParenthesesCombination.Solution(4)
+        };
+
+        public override string GetDescription()
+        {
+            return "Implement an algorithm to print all valid (properly open and closed) combinations of n pairs of parentheses.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
