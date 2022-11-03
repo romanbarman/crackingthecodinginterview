c8bf972a42b02b734be216eb4f818c33054c7ca3
Raman Zanko <r.zanko@godeltech.com>
2022-11-03 10:22:58 +0200

    Added solution for question 9 from chapter 10

---
+++ Ch 10. Sorting And Searching/Q10_09_Find_In_Matrix.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter10
+{
+    public class Q10_09_Find_In_Matrix : BaseQuestion
+    {
+        private static readonly ISolution[] solutions = new ISolution[]
+        {
+            new FindInMatrix.Solution(new int[,]
+            {
+                { 15, 20, 40, 85 },
+                { 20, 35, 80, 95 },
+                { 30, 55, 95, 105 },
+                { 40, 80, 100, 120 }
+            }, 55)
+        };
+
+        public override string GetDescription()
+        {
+            return "For a given matrix MxN, in which each row and column is sorted in ascending order, "
+                + "write a method for finding an element.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 10. Sorting And Searching/Q10_09_Find_In_Matrix/Coordinate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Chapter10.FindInMatrix
+{
+    public class Coordinate : ICloneable
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public bool IsInBounds(int[,] matrix)
+        {
+            return Row >= 0 && Column >= 0 && Row < matrix.GetLength(0) && Column < matrix.GetLength(1);
+        }
+
+        public bool IsBefore(Coordinate p)
+        {
+            return Row <= p.Row && Column <= p.Column;
+        }
+
+        public object Clone()
+        {
+            return new Coordinate(Row, Column);
+        }
+
+        public void SetToAverage(Coordinate min, Coordinate max)
+        {
+            Row = (min.Row + max.Row) / 2;
+            Column = (min.Column + max.Column) / 2;
+        }
+    }
+}
