15986721409338c8e96ef8f010fd5df3a061e296
Raman Zanko <r.zanko@godeltech.com>
2022-11-08 10:28:04 +0200

    Added solution for question 10 from chapter 10

---
+++ Ch 10. Sorting And Searching/Q10_10_Find_Rank.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter10
+{
+    public class Q10_10_Find_Rank : BaseQuestion
+    {
+        private ISolution[] solutions = new ISolution[]
+        {
+            new FindRank.Solution(new [] { 20, 15, 25, 10, 23, 5, 13, 24 }, 24)
+        };
+
+        public override string GetDescription()
+        {
+            return "You are processing a stream of integers. Periodically, you need to find the rank of x. "
+                + "Implement the track(int x) method, which is called when generating each character, "
+                + "and the getRankOfNumber(int x) method, which returns the number of values < x.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
---
+++ Ch 10. Sorting And Searching/Q10_10_Find_Rank/Solution.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using System;
+
+namespace Chapter10.FindRank
+{
+    public class Solution : ISolution
+    {
+        private readonly int[] array;
+        private readonly int rank;
+        private RankNode tree;
+
+        public Solution(int[] array, int rank)
+        {
+            this.array = array;
+            this.rank = rank;
+        }
+
+        public bool HasComment => false;
+
+        public string GetComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Array: {string.Join(", ", array)}");
+
+            foreach (var item in array)
+                Track(item);
+
+            Console.WriteLine($"Rank {rank} = {GetRankOfNumber(rank)}");
+        }
+
+        private void Track(int data)
+        {
+            if (tree == null)
+                tree = new RankNode(data);
+            else
+                tree.Insert(data);
+        } 
+
+        private int GetRankOfNumber(int number)
+        {
+            return tree.GetRank(number);
+        }
+    }
+}
