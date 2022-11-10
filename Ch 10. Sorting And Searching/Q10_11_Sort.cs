1a8cefde331ddbe7df466f4922510c402769606d
Raman Zanko <r.zanko@godeltech.com>
2022-11-10 10:36:56 +0200

    Added solution for question 11 from chapter 10

---
+++ Ch 10. Sorting And Searching/Q10_11_Sort.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using System.Collections.Generic;
+
+namespace Chapter10
+{
+    public class Q10_11_Sort : BaseQuestion
+    {
+        private static ISolution[] solutions = new ISolution[]
+        {
+            new Sort.Solution(new int[] { 1, 2, 3, 4, 5, 6 })
+        };
+
+        public override string GetDescription()
+        {
+            return "There is an array of integers. "
+                + "We will call a peak an element that is greater than neighboring elements or equal to them, "
+                + "and a trough is an element that is less than neighboring elements or equal to them. "
+                + "Sort an array of integers into alternating sequences of peaks and troughs.";
+        }
+
+        protected override IList<ISolution> GetSolutions()
+        {
+            return solutions;
+        }
+    }
+}
